5a65c495928527d9683795b40e0c0c53faf6c95c
Mythamor <bethmithamo.bm@gmail.com>
2023-12-16 22:48:54 +0300

    Create a SQL procedure with a conditional statement

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,29 +5,31 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
-    DECLARE total_score INT;
-    DECLARE total_projects INT;
-    DECLARE avg_score FLOAT;
+
+    DECLARE p_total_score INT;
+    DECLARE p_total_projects INT;
+    DECLARE p_avg_score FLOAT;
 
     -- Compute total score and projects for user
-    SELECT SUM(score), COUNT(project_id)
-    INTO total_score, total_projects
+    SELECT SUM(score),  COUNT(DISTINCT project_id)
+    INTO p_total_score, p_total_projects
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
+
 
     -- Compute average score
-    IF total_projects > 0 THEN
-        SET avg_score = total_score / total_projects;
+    IF p_total_projects > 0 THEN
+        SET p_avg_score = p_total_score / p_total_projects;
     ELSE
-        SET avg_score = 0;
+        SET p_avg_score = 0;
     END IF;
 
     -- Update the average_score for the user
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    SET average_score = p_avg_score
+    WHERE id = p_user_id;
 
 END $$
 
