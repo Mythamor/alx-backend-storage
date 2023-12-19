d60e00fa1b76a237e0c805fb6fc7f16ad00e6f99
Mythamor <bethmithamo.bm@gmail.com>
2023-12-19 23:31:03 +0300

    script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,6 +9,9 @@ from pymongo import MongoClient
 
 
 def log_stats():
+    """
+    script that provides some stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
