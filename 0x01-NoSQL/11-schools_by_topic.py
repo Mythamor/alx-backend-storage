08e6e3673d62a8b7cded7e506710bb27449a92c3
Mythamor <bethmithamo.bm@gmail.com>
2023-12-19 21:24:43 +0300

    function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Module: 11-schools_by_topic.py
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school having a specific topic:
+    """
+    query = {"topic": topic}
+    return mongo_collection.find(query)
