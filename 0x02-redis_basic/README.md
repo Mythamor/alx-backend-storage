07b8441a2708786905d392a07e14a4818e43cf24
Mythamor <bethmithamo.bm@gmail.com>
2023-12-21 00:49:06 +0300

    Writing strings in Redis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,15 @@
+# 0x02. Redis basic
+
+* Back-end
+* Redis
+
+## Learning Objectives
+Learn how to use redis for basic operations
+Learn how to use redis as a simple cache
+
+
+```
+$ sudo apt-get -y install redis-server
+$ pip3 install redis
+$ sed -i "s/bind .*/bind 127.0.0.1/g" /etc/redis/redis.conf
+```
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+Module: exercise.py
+        0. Writing strings to Redis
+"""
+
+
+import redis
+import uuid
+
+
+class Cache():
+    """
+    class Cache
+
+    Methods:__init__ 
+            store
+    """
+    def  __init__(self):
+        """
+        store an instance of the Redis client as a private variable
+        and flush the instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: 'Union[str, bytes, int, float]') -> str:
+        """
+        takes a data argument and returns a string
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
